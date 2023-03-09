d8704b9eb1e084bbe0cae0958808c0c2a9a26b6a
Phil Simpson <phil_simpson@att.net>
2023-03-08 20:59:32 -0600

    backup 3_8_23

--- components/GameDisplay.js
+++ components/GameDisplay.js
@@ -1,13 +1,50 @@
-import { View, Text, Image} from "react-native";
-import die_face_1 from '../assets/die_face_1_T.png';
+import { View, Text, Image, StyleSheet} from "react-native";
+import die_face_1 from '../assets/images/die_face_1_T.png';
+import die_face_2 from '../assets/images/die_face_2_T.png';
+import die_face_3 from '../assets/images/die_face_3_T.png';
+import die_face_4 from '../assets/images/die_face_4_T.png';
+import die_face_5 from '../assets/images/die_face_5_T.png';
+import die_face_6 from '../assets/images/die_face_6_T.png';
+import die_face_7 from '../assets/images/die_face_7_T.png';
 
 const GameDisplay = (props) => {
     let displayContent = null;
     const scoreCount = `Current Score Count: ${props.currentScore}`;
-    //source={`../assets/die_face_${props.dieFace}_T.png`}
-    // <Image 
-    //                 src={'die_face_1'}
-    //             />
+    let dieFace;
+
+    switch (props.dieFace) {
+        case 1:
+            dieFace = die_face_1;
+            break;
+        case 2:
+            dieFace = die_face_2;
+            break;
+        case 3:
+            dieFace = die_face_3;
+            break;
+        case 4:
+            dieFace = die_face_4;
+            break;
+        case 5:
+            dieFace = die_face_5;
+            break;
+        case 6:
+            dieFace = die_face_6;
+            break;
+        case 7:
+            dieFace = die_face_7;
+            break;
+        default:
+            break;
+    }
+    
+    const styles = StyleSheet.create({
+        dice: {
+            width: 100,
+            height: 100,
+        }
+    })
+    
 
     if (props.startGame && props.gameWinner) {
         displayContent = `Player ${props.currentPlayer} WINS!`;
@@ -23,7 +60,10 @@ const GameDisplay = (props) => {
                 {displayContent}
             </Text>
             <Text>
-                
+                <Image 
+                    style={styles.dice}
+                    source={dieFace}
+                />
             </Text>
             <Text>
                 {scoreCount}
